b5c386f6a98ba6103f68d41210ef4729dbd73df2
Nadrojria <loicdesbordes@yahoo.fr>
2025-07-24 09:22:00 +0200

    fix: fizzbuzz JS

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,14 @@
 const fizzbuzz = (number) => {
-  if (number % 3 == 0) {
-    return "Fizz"
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "Fizzbuzz";
   }
-  if (number === 5) {
-    return "Buzz"
+  if (number % 3 === 0) {
+    return "Fizz";
   }
-  return number
-}
+  if (number % 5 === 0) {
+    return "Buzz";
+  }
+  return number;
+};
 
 module.exports = { fizzbuzz }
